9c1d5d1972f5e70df23736b77a2e31d914036de8
장준태 <91938434+jangjuntae@users.noreply.github.com>
2024-06-02 20:11:19 +0900

    Update process.cpp

--- project_process/process.cpp
+++ project_process/process.cpp
@@ -1,127 +1,102 @@
 #include <iostream>
-#include <list>
-#include <memory>
-#include <mutex>
 #include <thread>
 #include <vector>
-#include <atomic>
-#include <chrono>
-#include <condition_variable>
-#include <map>
-#include <algorithm>
+#include <fstream>
 #include <sstream>
+#include <mutex>
+#include <map>
+#include <functional>
+#include <algorithm>
 
 using namespace std;
 
-class Process {
-public:
-    int id;
-    bool isForeground;
-    string command;
-    bool promoted = false;
-    int remainingTime = 0; // For wait queue simulation
+mutex cout_mutex;
 
-    Process(int id, bool isForeground, const string& command)
-        : id(id), isForeground(isForeground), command(command), promoted(false) {}
+void echo(const string& text) {
+    lock_guard<mutex> lock(cout_mutex);
+    cout << text << endl;
+}
 
-    string toString() const {
-        stringstream ss;
-        ss << "[" << id << (isForeground ? "F" : "B");
-        if (promoted) ss << "*";
-        ss << "]";
-        return ss.str();
+int gcd(int a, int b) {
+    while (b != 0) {
+        int t = b;
+        b = a % b;
+        a = t;
     }
-};
+    return a;
+}
 
-class DynamicQueue {
-private:
-    mutex mtx;
-    list<shared_ptr<Process>> processes;
-    map<int, shared_ptr<Process>> waitQueue;
+int count_primes(int limit) {
+    vector<bool> is_prime(limit + 1, true);
+    int count = 0;
 
-public:
-    void enqueue(shared_ptr<Process> process) {
-        lock_guard<mutex> lock(mtx);
-        processes.push_back(process);
-    }
-
-    void simulateSleep(int pid, int seconds) {
-        lock_guard<mutex> lock(mtx);
-        auto it = find_if(processes.begin(), processes.end(), [&pid](const shared_ptr<Process>& p) {
-            return p->id == pid;
-            });
-        if (it != processes.end()) {
-            (*it)->remainingTime = seconds;
-            waitQueue[pid] = *it;
-            processes.erase(it);
-        }
-    }
-
-    void wakeUpProcesses() {
-        lock_guard<mutex> lock(mtx);
-        for (auto it = waitQueue.begin(); it != waitQueue.end(); ) {
-            if (--it->second->remainingTime <= 0) {
-                enqueue(it->second);
-                it = waitQueue.erase(it);
-            }
-            else {
-                ++it;
+    for (int i = 2; i <= limit; ++i) {
+        if (is_prime[i]) {
+            ++count;
+            for (int j = 2 * i; j <= limit; j += i) {
+                is_prime[j] = false;
             }
         }
     }
+    return count;
+}
 
-    void display() {
-        lock_guard<mutex> lock(mtx);
-        cout << "Running: ";
-        if (!processes.empty()) {
-            cout << processes.front()->toString() << "\n";
-        }
-        cout << "DQ: P => ";
-        for (const auto& proc : processes) {
-            cout << proc->toString() << " ";
-        }
-        cout << "(bottom/top)\n";
-        cout << "---------------------------\n";
-        cout << "WQ: ";
-        for (const auto& wp : waitQueue) {
-            cout << wp.second->toString() << " - remaining time: " << wp.second->remainingTime << "s; ";
-        }
-        cout << "\n---------------------------\n";
+long long sum_up_to(int limit) {
+    long long sum = 0;
+    for (int i = 1; i <= limit; ++i) {
+        sum += i;
     }
-};
+    return sum % 1000000;
+}
 
-void shellProcess(DynamicQueue& dq, int interval) {
-    vector<string> commands = { "alarm clock", "todo list", "email check" };
-    int processId = 2; // Starting from 2, since 0 and 1 are taken by shell and monitor
-    for (const string& cmd : commands) {
-        auto proc = make_shared<Process>(processId++, true, cmd);
-        dq.enqueue(proc);
-        this_thread::sleep_for(chrono::seconds(interval));
-        dq.display();
+void execute_command(const string& command) {
+    istringstream iss(command);
+    string cmd;
+    iss >> cmd;
+
+    if (cmd == "echo") {
+        string text;
+        getline(iss, text);
+        echo(text.substr(1)); // remove leading space
+    }
+    else if (cmd == "gcd") {
+        int x, y;
+        iss >> x >> y;
+        lock_guard<mutex> lock(cout_mutex);
+        cout << "GCD of " << x << " and " << y << " is " << gcd(x, y) << endl;
+    }
+    else if (cmd == "prime") {
+        int x;
+        iss >> x;
+        lock_guard<mutex> lock(cout_mutex);
+        cout << "Count of primes up to " << x << " is " << count_primes(x) << endl;
+    }
+    else if (cmd == "sum") {
+        int x;
+        iss >> x;
+        lock_guard<mutex> lock(cout_mutex);
+        cout << "Sum of numbers up to " << x << " mod 1000000 is " << sum_up_to(x) << endl;
     }
 }
 
-void monitorProcess(DynamicQueue& dq, int interval) {
-    while (true) {
-        this_thread::sleep_for(chrono::seconds(interval));
-        dq.wakeUpProcesses();
-        dq.display();
+void process_commands(const string& filename) {
+    ifstream file(filename);
+    string line;
+    vector<thread> threads;
+
+    while (getline(file, line)) {
+        threads.push_back(thread(execute_command, line));
+        this_thread::sleep_for(chrono::seconds(5)); // simulate the time interval
+    }
+
+    // Wait for all threads to complete
+    for (auto& th : threads) {
+        th.join();
     }
 }
 
 int main() {
-    DynamicQueue dq;
-    auto shellProc = make_shared<Process>(0, true, "shell");
-    auto monitorProc = make_shared<Process>(1, false, "monitor");
-
-    dq.enqueue(shellProc);
-    dq.enqueue(monitorProc);
-
-    thread shellThread([&]() { shellProcess(dq, 5); });
-    thread monitorThread([&]() { monitorProcess(dq, 10); });
-
-    shellThread.join();
-    monitorThread.detach();
-
+    string filename = "commands.txt";
+    process_commands(filename);
     return 0;
 }
